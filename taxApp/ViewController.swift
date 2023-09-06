67fc7ceef4122645a9095d7efb14cf3f3f5a67e1
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-06 09:25:36 +0900

    Initial Commit

---
+++ taxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taxApp
+//
+//  Created by 小野寺良太 on 2023/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
